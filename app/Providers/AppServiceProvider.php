1ec4019a7b5e1e08001ba7584fc6431ee7805862
Nicolas Venegas <zx128kok@gmail.com>
2021-04-17 22:23:30 -0400

    circle db config - run migrations on heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // fix deploy issue on mysql with official recommended fix
+        Schema::defaultStringLength(191);
     }
 }
